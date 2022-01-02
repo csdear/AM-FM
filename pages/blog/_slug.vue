54c0c77abc4a10bef3b50a491bd721365023a849
csdear <csdear138@gmail.com>
2022-01-02 05:45:16 -0600

    default injected variables demo

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,17 +1,25 @@
 
 <template>
   <article>
+  <pre> {{ article }} </pre>
     <nuxt-content :document="article" />
+    <p>Post last updated: {{ formatDate(article.updatedAt) }}</p>
   </article>
 </template>
 
 <script>
-  export default {
-    async asyncData({ $content, params }) {
-      // fetch our article here
-      const article = await $content('articles', params.slug).fetch()
+export default {
+  async asyncData ({ $content, params }) {
+    // fetch our article here
+    const article = await $content('articles', params.slug).fetch()
 
-      return { article }
+    return { article }
+  },
+  methods: {
+    formatDate (date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
     }
   }
+}
 </script>
